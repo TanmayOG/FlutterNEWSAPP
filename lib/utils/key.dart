ae4c67c911daf7f0b8a9c05501e8cbcbce0969ca
TanmayOG <tanmayjha48gmail.com>
2022-02-18 18:56:53 +0530

    changes

--- lib/utils/key.dart
+++ lib/utils/key.dart
@@ -1,4 +1,4 @@
 class Key {
-  static String key = '2c5c87ed1d9f4633bd35459e8c501990';
-  static String key2 = 'c33be129af5564293f1bdae9bf9eb82e';
+  static String key = 'NEWS API KEY';
+  static String key2 = 'NEWS API KEY2';
 }
